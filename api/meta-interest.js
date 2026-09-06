f13559ce16f1c10d9a2a88b580d34553c54c1c4a
ngeawsrii-art <ngeawsrii@gmail.com>
2026-09-06 23:03:23 +0700

    meta-interest.js

--- api/meta-interest.js
+++ api/meta-interest.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
   try {
-    const { q, limit } = req.query;
+    const q = req.query.q;
+    const limit = req.query.limit || "10";
 
     if (!q) {
       return res.status(400).json({
@@ -16,14 +17,12 @@ export default async function handler(req, res) {
       });
     }
 
-    const resultLimit = limit ? parseInt(limit, 10) : 10;
-
     const url =
-      https://graph.facebook.com/v19.0/search +
-      ?type=adinterest +
-      &q=${encodeURIComponent(q)} +
-      &limit=${resultLimit} +
-      `&access_token=${FB_TOKEN}`;
+      "https://graph.facebook.com/v19.0/search" +
+      "?type=adinterest" +
+      "&q=" + encodeURIComponent(q) +
+      "&limit=" + encodeURIComponent(limit) +
+      "&access_token=" + encodeURIComponent(FB_TOKEN);
 
     const fbResponse = await fetch(url);
     const data = await fbResponse.json();
