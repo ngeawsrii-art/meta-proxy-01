21860cd82846f38374ca1a8a5db9f2eeaf08762b
ngeawsrii-art <ngeawsrii@gmail.com>
2026-09-09 22:58:42 +0700

    index.js

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ export default async function handler(req, res) {
       });
     }
 
-    const resultLimit = limit ? parseInt(limit, 10) : 10;
+    const resultLimit = Math.min(
+  limit ? parseInt(limit, 10) : 10,
+  10
+);
 
     const url = `https://graph.facebook.com/v19.0/search?type=adinterest&q=${encodeURIComponent(
       q
